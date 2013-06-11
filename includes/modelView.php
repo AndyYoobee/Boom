da37c1bc38606a8b76cf074ebc931be56e6acb98
unknown <andres.garcia@44383-WEB-WIN.auckland.natcoll.ac.nz>
2013-06-12 10:34:04 +1200

    ModelView

---
+++ includes/modelView.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once("maker.php");
+
+/*
+
+-----------------IMPORTANT---------------------
+
+//Renders the HTML for a Model
+	 !!!!!!! IT REQUIRES a MODEL OBJECT AND a MAKE OBJECT !!!!!!!
+	(respectively "model.php" and "maker.php" files) to be passed in for rendering.
+
+Methods of the ModelView class:
+
+** render ** : render the HTML about a selected model
+
+*/
+
+
+class ModelView{
+
+	public function render($oModel,$oMake){
+
+		$sHTML ="";
+
+		$sHTML .= '<div class="shell">
+				   <div id="pageContent"><h1>'.$oMake-> MakeName.'</h1>
+				   <h2>'.$oModel-> ModelName.'</h2>
+				   <h3>'.$oModel-> Price.'</h3>
+				   <p>'.$oModel-> Description.'</p>
+				   </div><!-- end of page content -->
+				   <div id="pageImage">
+                    <img src="600x300.png" height="300px" width="600px" alt="" />
+	                </div>
+	                <div id="pluscart"></div> <!-- PLACHOLDER --> 
+	                <div id="clear"></div>
+	                </div> <!-- end of shell -->';
+
+
+	    return $sHTML;
+
+	}
+
+	
+
+}
+
+/*
+$oModel= new Model();
+$oMake= new Make();
+
+$oModel-> load(19);
+$oMake-> load(3);
+
+$ModelView = new ModelView();
+echo $ModelView-> render($oModel,$oMake);
+*/
+
+?>
